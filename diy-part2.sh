3a308fe34c92b31bc79f1170bbf31034e4f09609
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-04-03 09:15:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
+git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
+git clone https://github.com/lucikap/luci-app-ua2f.git package/luci-app-ua2f
+git clone https://github.com/lucikap/luci-app-nettask.git package/luci-app-nettest
+git clone https://github.com/nihaoa15555/mac1.git package/luci-mac
