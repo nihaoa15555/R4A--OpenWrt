3cd22efe8ac37883f0af40e06c762867f2e2c4f7
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-08-27 17:09:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#rm -rf feeds/small8/miniupnpd-iptables && rm -rf package/feeds/small8/miniupnpd-iptables && ./scripts/feeds update -a && ./scripts/feeds install -a
-git clone https://github.com/Zxilly/UA2F.git package/UA2F
-git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
-git clone https://github.com/nihaoa15555/luci-app-ua2f.git package/luci-app-ua2f
-git clone https://github.com/nihaoa15555/luci-app-nettask.git package/luci-app-nettest
-git clone https://github.com/nihaoa15555/mac1.git package/luci-mac
 
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# delete conflict application
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+
+# use golang 1.22.x
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
