065861b2f1b0796444a53d526d8cbb2c18d4e803
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-09-09 15:55:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #rm -rf feeds/small8/miniupnpd-iptables && rm -rf package/feeds/small8/miniupnpd-iptables && ./scripts/feeds update -a && ./scripts/feeds install -a
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
+git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
+git clone https://github.com/nihaoa15555/luci-app-ua2f.git package/luci-app-ua2f
+git clone https://github.com/nihaoa15555/luci-app-nettask.git package/luci-app-nettest
+git clone https://github.com/nihaoa15555/mac1.git package/luci-mac
